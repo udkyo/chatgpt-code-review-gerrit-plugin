1490df66942ad57ac46686cf34e9f6fc00fab858
Patrizio <patrizio.gelosi@amarulasolutions.com>
2024-05-17 16:31:21 +0200

    Fix regression: missing eventHandlerTask.MODULE
    A regression introduced removing `eventHandlerTask.MODULE`,
    which was introduced in commit 00ae2f5 ("Decouple thread pool from event
    handler"), but not removing the call to install that module, has been
    corrected.
    
    Fixes: ec1cf3b ("Bind GerritChange in event context")
    Change-Id: I634de5a09bde7e0a3cfa2cb1e5b27618d0ae535b
    Signed-off-by: Patrizio <patrizio.gelosi@amarulasolutions.com>

--- src/main/java/com/googlesource/gerrit/plugins/chatgpt/Module.java
+++ src/main/java/com/googlesource/gerrit/plugins/chatgpt/Module.java
@@ -3,15 +3,12 @@ package com.googlesource.gerrit.plugins.chatgpt;
 import com.google.gerrit.server.events.EventListener;
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
-import com.googlesource.gerrit.plugins.chatgpt.listener.EventHandlerTask;
 import com.googlesource.gerrit.plugins.chatgpt.listener.GerritListener;
 
 public class Module extends AbstractModule {
 
     @Override
     protected void configure() {
-        install(EventHandlerTask.MODULE);
-
         Multibinder<EventListener> eventListenerBinder = Multibinder.newSetBinder(binder(), EventListener.class);
         eventListenerBinder.addBinding().to(GerritListener.class);
     }
